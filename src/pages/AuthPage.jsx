014b2aa8f4a2aeb947c8bb9bae8e887e7dd1100f
BlueJack14 <bluejack1414@gmail.com>
2023-12-15 23:05:43 +0100

    login and password validate

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
-
-
 const AuthPage = () => { 
 
-    const [login,setLogin] = useState(null)
-    const [passowrd,setPassword] = useState(null)
+    
+    const [_login,setLogin] = useState(null)
+    const [_passowrd,setPassword] = useState(null)
+
+    function login(){
+
+    }
+
+    function checkInputs(){
+        if(_login==="admin"&&_passowrd==="admin")
+            console.log("podano dobre dane")          
+        else
+            console.log("podano złe dane")    
+    }
       return(
         <>  <div>
                 <div>
                     <input type="text" onChange={event=> setLogin(event.target.value)}/>
                     <input type="password" onChange={event=> setPassword(event.target.value)}/>
-                    <input type="button" value="Log in" />
+                    <input type="button" onClick={()=>checkInputs()} value="Log in" />
                 </div>
                 <p></p>
             </div></>           
