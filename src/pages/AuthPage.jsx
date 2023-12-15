1d37b1790ad70710b471cae41553b6321135817a
BlueJack14 <bluejack1414@gmail.com>
2023-12-15 22:36:17 +0100

    auth page done

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 
-const AuthPage = () => {
-    return(
-        <></>
+
+
+const AuthPage = () => { 
+
+    const [login,setLogin] = useState(null)
+    const [passowrd,setPassword] = useState(null)
+      return(
+        <>  <div>
+                <div>
+                    <input type="text" onChange={event=> setLogin(event.target.value)}/>
+                    <input type="password" onChange={event=> setPassword(event.target.value)}/>
+                    <input type="button" value="Log in" />
+                </div>
+                <p></p>
+            </div></>           
     );
 }
 
