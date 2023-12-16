2875efa2b40838e8acde6dfc9af709489a0fb6a8
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-12-16 17:02:03 +0100

    Added calendar

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,12 +1,13 @@
 import Calendar from "react-calendar";
+import '../Calendar.css'
 import { useState } from "react";
 
 const CalendarPage = () => {
     const [ currentDate, setCurrentDate ] = useState(new Date());
 
     return(
-        <div>
-            <Calendar value={currentDate}/>
+        <div className="text-black flex justify-center h-[100vh] items-center">
+            <Calendar onChange={setCurrentDate} value={currentDate} view="month"/>
         </div>
     );
 }
