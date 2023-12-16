e37c52c7943ae308ff5d2db14f966c4e694daff0
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-12-16 17:45:12 +0100

    modal

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import taskDetailsReducer from './taskDetailsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    taskDetails: taskDetailsReducer,
   }
 });
 
