2a530548e8b0c4a5e79656fe1e2c54e8a274a277
Slawek Galka <galka.slawek6@gmail.com>
2023-12-16 02:27:54 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AuthPage from "./pages/AuthPage";
+import Navbar from "./components/Navbar";
+import {useSelector} from "react-redux";
 
 const App = () => {
+    const user = useSelector(state => state.user);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthPage/>}/>
       </Routes>
+        {/*{user.id && <Navbar/>}*/}
+        <Navbar/>
     </BrowserRouter>
   );
 }
